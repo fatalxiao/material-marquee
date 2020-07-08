b20c5719c69ce69e88f8c4c17b237cad3ec5d9a4
liangxiaojun <fatalxiao@163.com>
2020-07-08 14:13:26 +0800

    update Brand

---
+++ src/modules/brand/Brand.js
@@ -0,0 +1,20 @@
+/**
+ * @file Brand.js
+ * @author liangxiaojun
+ */
+
+import React from 'react';
+
+// Styles
+import 'scss/brand/Brand.scss';
+
+function Brand() {
+    return (
+        <div className="brand">
+            <div className="brand-logo"></div>
+            <div className="brand-name"></div>
+        </div>
+    );
+}
+
+export default Brand;
