1e1e32212a12d407b3be87105c3a33289cde27f6
liangxiaojun <fatalxiao@163.com>
2020-07-15 15:46:55 +0800

    update electron

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 const {app, BrowserWindow} = require('electron');
 
+console.log(`${__dirname}/docs/index.html`);
+
 function createWindow() {
+
     // 创建浏览器窗口
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        show: false,
+        fullscreen: true,
+        frame: false,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
+    win.once('ready-to-show', () => win.show());
+
     // 并且为你的应用加载index.html
-    win.loadFile('index.html');
+    win.loadFile(`${__dirname}/docs/index.html`);
 
     // 打开开发者工具
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
+
 }
 
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
